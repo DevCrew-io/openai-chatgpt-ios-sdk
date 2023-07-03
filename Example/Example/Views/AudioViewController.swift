a5ae39d004e8aed72df87ae7b76ec98bda869479
Ghullam Abbas <ghullam.abbas@devcrew.io>
2023-07-03 16:04:40 +0500

    feat: add audio translation and transcription in sample project

--- Example/Example/Views/AudioViewController.swift
+++ Example/Example/Views/AudioViewController.swift
@@ -9,13 +9,53 @@ import UIKit
 import ChatGPTAPIManager
 
 class AudioViewController: UIViewController {
-
+    
+    @IBOutlet weak var textView: UITextView!
+    let url = Bundle.main.url(forResource: "Boswell Interview - Ted 2023-05-16", withExtension: "m4a")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
     }
-  
-   
-
+    @IBAction func audioTranslation(sender: UIButton) {
+        let fileURL = URL(fileURLWithPath: url!.path)
+        self.audioTranslation(url: fileURL)
+    }
+    @IBAction func audioTranscription(sender: UIButton) {
+        let fileURL = URL(fileURLWithPath: url!.path)
+        self.audioTranscriptions(url: fileURL)
+    }
+    
+    func audioTranslation(url: URL) {
+        EZLoadingActivity.show("Loading...", disableUI: true)
+        ChatGPTAPIManager.shared.audioTranslationRequest(fileUrl: url,temperature: 0.8, model: .whisper1, endPoint: .translations, completion: { result in
+            
+            switch result {
+            case.success(let success):
+                print(success)
+                DispatchQueue.main.async {
+                    self.textView.text =  success
+                }
+            case.failure(let error):
+                print(error)
+            }
+        })
+    }
+    func audioTranscriptions(url: URL) {
+        EZLoadingActivity.show("Loading...", disableUI: true)
+        ChatGPTAPIManager.shared.audioTranscriptionRequest(fileUrl: url, prompt: "Translate this audio", model: .whisper1, endPoint: .transcriptions, completion: { result in
+            
+            switch result {
+            case.success(let success):
+                print(success)
+                DispatchQueue.main.async {
+                    EZLoadingActivity.hide(true,animated: true)
+                    self.textView.text =  success
+                }
+            case.failure(let error):
+                print(error)
+                EZLoadingActivity.hide(false,animated: true)
+            }
+        })
+    }
 }
